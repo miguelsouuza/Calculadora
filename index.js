d24190cc29903ab9f89e3927fdf0e9bce6fd77af
miguelsouuza <miguel68644794@hotmail.com>
2022-08-22 15:21:06 -0300

    funcao de limpar em javascript

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function insert(num){
     let numero = document.querySelector(`#resultado`).innerHTML
     document.querySelector(`#resultado`).innerHTML=numero+num
 }
+function limpar(){
+    document.querySelector(`#resultado`).innerHTML=""
+}

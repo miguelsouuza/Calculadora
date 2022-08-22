7ec249a08af080e656f86e12a8aedb24db54b527
miguelsouuza <miguel68644794@hotmail.com>
2022-08-22 15:25:24 -0300

    Apagando um dado por vez

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function insert(num){
 function limpar(){
     document.querySelector(`#resultado`).innerHTML=""
 }
+function apagar(){
+   var resultado= document.querySelector(`#resultado`).innerHTML
+   /* subtring pega todos os caracteres dentro do paragrafo e diminui em um*/
+   document.querySelector(`#resultado`).innerHTML= resultado.substring(0,resultado.length -1)
+}
